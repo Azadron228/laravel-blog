6b32bd94eb260613a3a7e4eea6e8eed23a4b4871
Azardo5300 <ibrayv2005@gmail.com>
2023-07-14 14:45:34 +0600

    login and registration added

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -21,6 +23,11 @@ Route::get('/', function () {
     return 'Hello World';
 });
 
+Route::get('register', [RegistrationController::class, 'create'])->name('register');
+Route::post('register', [RegistrationController::class, 'store']);
+Route::get('login', [AuthController::class, 'create'])->name('login');
+Route::post('login', [AuthController::class, 'store']);
+
 Route::prefix('posts')->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('posts.index');
     Route::get('/{id}', [PostController::class, 'show'])->name('posts.show');
@@ -31,7 +38,9 @@ Route::prefix('posts')->group(function () {
 
 Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
 
-Route::middleware('auth.basic')->group(function () {
+Route::middleware('auth')->group(function () {
+  Route::get('/profile', [UserController::class, 'edit'])->name('profile.edit');
+  Route::delete('/profile', [UserController::class, 'destroy'])->name('profile.destroy');
   Route::get('/profile', [UserController::class, 'profile'])->name('profile');
   
 });
@@ -39,10 +48,17 @@ Route::post('/users/update-avatar', [UserController::class, 'updateAvatar'])->na
 Route::get('/update-avatar', [UserController::class, 'showUpdateAvatarForm'])->name('update.avatar.form');
 Route::post('/update-avatar', [UserController::class, 'updateAvatar'])->name('update.avatar');
 
-Route::get('/login', [LoginController::class, 'show'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+// Route::get('/login', [LoginController::class, 'show'])->name('login');
+// Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 
 Route::get('/file', function () {
     return response()->download(storage_path('app/jojo.jpg'));
 });
 
+// Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+// Route::post('/register', [AuthController::class, 'register']);
+//
+// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+// Route::post('/login', [AuthController::class, 'login']);
+//
+// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
